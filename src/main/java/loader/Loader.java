ac4cf17321abf14fa7f1f1075a762cafbedaaa1f
Aleksey Kislov <Aleksey_kislov@epam.com>
2018-10-15 16:10:06 +0300

    realize TsvLoader

--- src/main/java/loader/Loader.java
+++ src/main/java/loader/Loader.java
@@ -2,8 +2,9 @@ package loader;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
 import java.io.Reader;
 
 public interface Loader<T> {
-  T load(Reader reader) throws JAXBException, XMLStreamException;
+  T load(Reader reader) throws JAXBException, XMLStreamException, IOException;
 }
--- src/main/java/loader/TsvLoader.java
+++ src/main/java/loader/TsvLoader.java
@@ -1,10 +1,19 @@
 package loader;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TsvLoader implements Loader<List<String[]>> {
-  public List<String[]> load(Reader reader) {
-    return null;
+  public List<String[]> load(Reader reader) throws IOException {
+    List<String[]> strings = new ArrayList<>();
+    BufferedReader bufferedReader = new BufferedReader(reader);
+    String buffer;
+    while ((buffer = bufferedReader.readLine()) != null) {
+      strings.add(buffer.split("\t"));
+    }
+    return strings;
   }
 }
